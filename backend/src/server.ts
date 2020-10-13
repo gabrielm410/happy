2258fa3a459c9bb0cb86eb58406ba93b70aadabc
Gabriel Martins <gabrielfmartins410@gmail.com>
2020-10-13 20:10:36 -0300

    added data validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'; // express ajuda a lidar com requisição e resposta
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database/connection';
@@ -8,6 +9,7 @@ import errorHandler from './errors/handler';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
