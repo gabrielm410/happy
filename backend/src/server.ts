382e7989b15569b29c29c7c6c1cd4b51b3715425
Gabriel Martins <gabrielfmartins410@gmail.com>
2020-10-13 13:12:19 -0300

    started backend

---
+++ backend/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express'; // express ajuda a lidar com requisição e resposta
+
+const app = express();
+
+app.get('/users', (request, response) => {
+
+});
+
+app.listen(3333); // localhost:3333
