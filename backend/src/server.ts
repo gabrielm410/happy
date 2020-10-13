b22eb6756539f392455192a7b5b2b7a64a0edf9b
Gabriel Martins <gabrielfmartins410@gmail.com>
2020-10-13 19:50:02 -0300

    added error validation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,17 @@
 import express from 'express'; // express ajuda a lidar com requisição e resposta
 import path from 'path';
+import 'express-async-errors';
 
 import './database/connection';
 import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler);
 
 // Rota = conjunto
 // Recurso = usuario
