2dd3b6321f1a8853f2447ad8d7a71ef6593d9afa
Gabriel Martins <gabrielfmartins410@gmail.com>
2020-10-14 15:35:26 -0300

    created abstract components

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -2,22 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
-import Leaflet from 'leaflet';
-
-import 'leaflet/dist/leaflet.css';
-
 
 import mapMarkerImg from '../images/map-marker.svg';
-
+import mapIcon from '../utils/mapIcon';
 import '../styles/pages/orphanages-map.css';
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
 
 function OrphanagesMap() {
     return (
@@ -31,7 +19,7 @@ function OrphanagesMap() {
                 </header>
 
                 <footer>
-                    <strong> Sâo José do Rio Preto </strong>
+                    <strong> São José do Rio Preto </strong>
                     <span> São Paulo </span>
                 </footer>
             </aside>
